2a974b1365587e622e210258796afd6aeade058e
ShinyaKato <k.shin220@gmail.com>
2021-01-10 17:38:13 +0900

    Enable SCSS syntax

--- src/pages/[repo_name].tsx
+++ src/pages/[repo_name].tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.scss"
 
 interface GitHubRepo {
   id: string;
